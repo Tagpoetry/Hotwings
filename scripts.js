e2113fbdd567211efd4b983ded15141789350a50
nicole nelson <tagpoetry@gmail.com>
2019-10-29 21:18:07 -0500

    Made changes to photo slide show

--- scripts.js
+++ scripts.js
@@ -1,21 +1,28 @@
-// active carousel navigation
-@for $i from 1 through map_get($css-carousel, slides) {
-    #carousel-#{$i}:checked ~ .carousel-nav label[for="carousel-#{$i}"] {
-        background-color: #fff;
-    }
-}
- 
-// active carousel slides
-@for $i from 1 through map_get($css-carousel, slides) {
-    #carousel-#{$i}:checked ~ .carousel-slides .carousel-inner {
-        margin-left: (-100% * ($i - 1));
-    }
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-
-
-
-
-
-
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
